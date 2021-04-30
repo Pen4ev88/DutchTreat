5c82e982500d51e0eeac8d13965e1f8f31acccfa
Pen4ev88 <m.pen4ev88@gmail.com>
2021-04-30 11:12:36 +0300

    First commit

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,60 @@
+﻿using DutchTreat.Servces;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this._mailService = mailService;
+        }
+
+
+        public IActionResult Index()
+        {
+            //throw new InvalidProgramException("Bad things happend to good developers!");
+            return View();
+        }
+
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //send the mail
+                _mailService.SendMessage("m_pen4ev@abv.bg", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                //shor error
+            }
+
+            return View();
+        }
+
+        //[HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("Hello Plurasight");
+
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on" + $(this).text)
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $poputForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $poputForm.slideToggle(500);
+    });
+
+});
